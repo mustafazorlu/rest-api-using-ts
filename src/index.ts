3e6a3b4ffd6ab67bcc93da8d018d5373ef2a1ec4
Mustafa Zorlu <mzorlu3579@gmail.com>
2025-03-13 21:23:32 +0300

    Add Express server setup and update project configuration

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("hello typescript")
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import cors from "cors";
+
+const app = express();
+
+app.use(cors({
+    credentials:true
+}))
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+
+console.log("selam");
